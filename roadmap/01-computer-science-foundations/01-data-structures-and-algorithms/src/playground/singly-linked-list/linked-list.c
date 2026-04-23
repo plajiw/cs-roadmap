0b4c24d32fa18dd0f0a4733e8360f5f541d30f7c
plajiw <pabloribeiro.cont@gmail.com>
2026-04-22 23:32:28 -0300

    atividade-liked-list

--- roadmap/01-computer-science-foundations/01-data-structures-and-algorithms/src/playground/singly-linked-list/linked-list.c
+++ roadmap/01-computer-science-foundations/01-data-structures-and-algorithms/src/playground/singly-linked-list/linked-list.c
@@ -2,6 +2,9 @@
 
 void initLinkedList(LinkedList *list)
 {
+    list->begin = NULL;
+    list->end = NULL;
+    list->size = 0;
 }
 
 void emptyLinkedList(LinkedList *list)
@@ -10,15 +13,45 @@ void emptyLinkedList(LinkedList *list)
 
 bool isEmpty(LinkedList *list)
 {
-    return false;
+    return list->size == 0;
 }
 
 void pushBegin(LinkedList *list, int item)
 {
+    Node *newNode = createNode(item, NULL);
+
+    if (newNode == NULL)
+    {
+        printf("[Erro] - Falha na alocação de memória\n");
+        return;
+    }
+
+    newNode->reference = list->begin;
+    list->begin = newNode;
+
+    if (isEmpty(list))
+        list->end = newNode;
+
+    list->size++;
 }
 
 void pushEnd(LinkedList *list, int item)
 {
+    Node *newNode = createNode(item, NULL);
+
+    if (newNode == NULL)
+    {
+        printf("[Erro] - Falha na alocação de memória\n");
+        return;
+    }
+
+    if (isEmpty(list))
+        list->begin = newNode;
+    else
+        list->begin->reference = newNode;
+
+    list->end = newNode;
+    list->size++;
 }
 
 void pushPos(LinkedList *list, int item, int pos)
